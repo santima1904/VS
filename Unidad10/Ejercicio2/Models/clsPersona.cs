66df5516dec9e76ec527097a5ef664cdfa219deb
Santi <santima1904@gmail.com>
2021-10-25 14:36:49 +0200

    avanzado

---
+++ Unidad10/Ejercicio2/Models/clsPersona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio2.Models
+{
+    public class clsPersona
+    {
+        #region atributos privados
+        private String nombre;
+        private String apellidos;
+        #endregion
+
+        #region propiedades publicas
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+
+
+
+
+        #endregion
+
+        // Forma de autoimplementacion (para más adelante)
+        // public String Nombre { get; set; }
+
+        #region constructores
+        //Constructor por defecto
+        public clsPersona()
+        {
+            this.nombre = "Santi";
+            this.apellidos = "Martinez";
+        }
+
+        //Constructor por parametros
+        public clsPersona(string nombre, string apellidos)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+
+        #endregion
+    }
+} 
---
+++ Unidad10/Ejercicio2/ViewModels/Ejercicio2VM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio2.Models;
+
+namespace Ejercicio2.ViewModels
+{
+    public class Ejercicio2VM
+    {
+        clsPersona oPersona;
+        public clsPersona OPersona { get => oPersona; set => oPersona = value; }
+    }
+}
